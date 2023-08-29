9b659146b7a16664f186ee4036a2ca9aadbd5bbd
Vinicius <vinicius.prado@nexuscloud.com.br>
2023-08-28 21:12:14 -0300

    Initial commit

---
+++ src/test/java/vinicius/prado/Todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package vinicius.prado.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
